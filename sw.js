ddac3acc461770d80456f0ceee2b5ddb1ba6acda
ruisacai <85042737+ruisacai@users.noreply.github.com>
2026-09-19 09:49:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // un acceso directo). Cachea el "cascarón" de la app para que cargue
 // más rápido, pero la traducción sigue necesitando conexión a internet.
 
-const CACHE_NAME = "poliglot-cache-v1";
+const CACHE_NAME = "poliglot-cache-v2";
 const APP_SHELL = [
   "./",
   "./index.html",
